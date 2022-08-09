be109d37085f85ca178afb8c1d79c50ecfc5f855
崔桂铭 <guiming.cui@qlchat.com>
2022-08-09 19:32:17 +0800

    ci: 初始化

---
+++ bin/index.js
@@ -0,0 +1,59 @@
+#! /usr/bin/env node
+// 为了方便测试
+const path = require("path");
+const program = require("commander");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const ora = require("ora");
+const validTypes = ["react", "vue"];
+const fs = require("fs-extra");
+
+program
+  .command("create <project-name>") // 增加创建指令
+  .description("create a new project") // 添加描述信息
+  .action((projectName, cmd) => {
+    // 处理用户输入create 指令附加的参数
+    require("../lib/create")(projectName, cmd);
+  });
+
+program
+  .command("config [value]")
+  .description("inspect and modify the config")
+  .option("-g, --get <key>", "get value by key")
+  .option("-s, --set <key> <value>", "set option[key] is value")
+  .option("-d, --delete <key>", "delete option by key")
+  .action((value, keys) => {
+    console.log(value, keys);
+  });
+
+program.on("--help", function () {
+  console.log(
+    "\r\n" +
+      figlet.textSync("void-cli", {
+        font: "3D-ASCII",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        width: 80,
+        whitespaceBreak: true,
+      })
+  );
+  // 前后两个空行调整格式，更舒适
+  console.log();
+  console.log(
+    `Run ${chalk.cyan(
+      "zc-cli <command> --help"
+    )} for detailed usage of given command.`
+  );
+  console.log();
+});
+
+program
+  .name("zc-cli")
+  .usage(`<command> [option]`)
+  .version(`zc-cli ${require("../package.json").version}`);
+
+// 解析用户执行时输入的参数
+// process.argv 是 nodejs 提供的属性
+// npm run server --port 3000
+// 后面的 --port 3000 就是用户输入的参数
+program.parse(process.argv);
